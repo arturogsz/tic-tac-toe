a3751712a576343ebeb459f46cb84d9a6429991e
Arturo Garcia <arturo.garcia.9204@gmail.com>
2018-03-03 15:37:33 -0600

    restructure the files and folders to have all components under a single file and functions under another one

---
+++ src/components/game.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import Board from './board';
+import calculateWinner from '../functions/calculateWinner'
+
+  class Game extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        history: [{
+          squares: Array(9).fill(null),
+        }],
+        order: Array(9).fill(0),
+        stepNumber: 0,
+        xIsNext: true,
+      };
+}
+
+    handleClick(i) {
+      const history = this.state.history.slice(0, this.state.stepNumber + 1);
+      const current = history[history.length - 1];
+      const squares = current.squares.slice();
+      const order = this.state.order ;
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
+      // Aqui se asigna el valor X / O al cuadro clickado
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      //console.log(this.state.stepNumber);
+      order[this.state.stepNumber] = i;
+      this.setState({
+        history: history.concat([{
+          squares: squares,
+        }]),
+        order: order,
+        stepNumber: history.length,
+        xIsNext: !this.state.xIsNext,
+      });
+    }
+
+    jumpTo(step) {
+      this.setState({
+        stepNumber: step,
+        xIsNext: (step % 2) === 0,
+      });
+    }
+
+    render() {
+      const history = this.state.history;
+      const current = history[this.state.stepNumber];
+      const winner = calculateWinner(current.squares);
+      const gridStrings = ["0,0", "1,0", "2,0",
+                           "0,1", "1,1", "2,1",
+                           "0,2", "1,2", "2,2"];
+      const moves = history.map((step, move) => {
+        const location = gridStrings[this.state.order[move - 1]];
+        const desc = move ?
+          'Go to move #' + move + ' ' + location :
+          'Go to game start';
+        return (
+          <li key={move}>
+            <button onClick={() => this.jumpTo(move)}>{desc}</button>
+          </li>
+        );
+      });
+
+      let status;
+      if (winner) {
+        status = 'Winner: ' + winner;
+      } else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
+
+      return (
+        <div className="game">
+          <div className="game-board">
+            <Board
+              squares={current.squares}
+              onClick={(i) => this.handleClick(i)}
+            />
+          </div>
+          <div className="game-info">
+            <div>{status}</div>
+            <ol>{moves}</ol>
+          </div>
+        </div>
+      );
+    }
+  }
+
+  export default Game;
